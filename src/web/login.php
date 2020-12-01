cb9e5ddbe1933208f0a26a113f87583b9d56deac
FJ <fsanchez@lsi.uned.es>
2020-12-01 14:04:10 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/web/login.php
+++ src/web/login.php
@@ -7,7 +7,7 @@ echo TOOL_HOST . "/launch.php" . "?target_link_uri=" . TOOL_PARAM;
 use \IMSGlobal\LTI;
 
 LTI\LTI_OIDC_Login::new(new Example_Database())
-    //->do_oidc_login_redirect(TOOL_HOST . "/game.php")
-    ->do_oidc_login_redirect(TOOL_HOST . "/launch.php" . "?target_link_uri=" . TOOL_PARAM)
+    ->do_oidc_login_redirect(TOOL_HOST . "/game.php")
+    //->do_oidc_login_redirect(TOOL_HOST . "/launch.php" . "?target_link_uri=" . TOOL_PARAM)
     ->do_redirect();
 ?>
