e5936884dabc672c1075570b908ab7274b12f7c9
FJ <fsanchez@lsi.uned.es>
2020-12-01 14:11:48 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/web/login.php
+++ src/web/login.php
@@ -2,8 +2,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../db/example_database.php';
 
-echo TOOL_HOST . "/launch.php" . "?target_link_uri=" . TOOL_PARAM;
-
 use \IMSGlobal\LTI;
 
 LTI\LTI_OIDC_Login::new(new Example_Database())
