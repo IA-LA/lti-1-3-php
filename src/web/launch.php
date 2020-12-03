e33ad57bcfd23e9193ee671e53eded32fd9dd271
FJ <fsanchez@lsi.uned.es>
2020-12-03 13:47:23 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/web/launch.php
+++ src/web/launch.php
@@ -7,7 +7,19 @@ use \IMSGlobal\LTI;
 $launch = LTI\LTI_Message_Launch::new(new Example_Database())
     ->validate();
 
-echo '<iframe id="frame" src="' . TOOL_PARAM . '"></iframe>';
+echo '<iframe id="frame" src="' . TOOL_PARAM . '"   style="
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    right: 0px;
+    width: 100%;
+    border: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    z-index: 999999;
+    height: 100%;
+  "></iframe>';
 
 ?>
 
