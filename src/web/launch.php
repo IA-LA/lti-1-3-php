a39a3aa7c35a47f948aee4440b63e88b9b28d6f7
FJ <fsanchez@lsi.uned.es>
2020-12-03 13:39:34 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/web/launch.php
+++ src/web/launch.php
@@ -7,11 +7,13 @@ use \IMSGlobal\LTI;
 $launch = LTI\LTI_Message_Launch::new(new Example_Database())
     ->validate();
 
-echo '<iframe id="frame" src="https://www.bing.es" onload="document.getElementById(\'frame\').src=\'\'"><\/iframe>'
+echo '<iframe id="frame" src="https://www.bing.es" onload="document.getElementById(\'frame\').src=\'\'"></iframe>'
 
 ?>
 
-<!-- <iframe id="frame" src="https://www.bing.es" onload="document.getElementById('frame').src=''"></iframe> -->
+<!--
+    <iframe id="frame" src="https://www.bing.es" onload="document.getElementById('frame').src=''"></iframe>
+-->
 
 <?php
     if ($launch->is_deep_link_launch()) {
