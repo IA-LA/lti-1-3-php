1eabe5140237082ecbb3e3598aacff98d70e35b5
FJ <fsanchez@lsi.uned.es>
2020-12-06 17:06:54 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/db/example_database.php
+++ src/db/example_database.php
@@ -46,14 +46,14 @@ var_dump(stream_get_meta_data($stream));
 // Resultado
 $json_obj = json_decode(stream_get_contents($stream));
 var_dump($json_obj);
-echo $json_obj->{'data'};
+echo $json_obj->{'data'}->{'0'}->{'launch_parameters'}->{'0'};
 if($json_obj->{'result'} === "ok"){
     // Registro
     //$json_obj_registro = json_decode($json_obj->{'data'});
     //echo $json_obj_registro->{'launch_parameters'};
     //$_SESSION['iss'] = [$json_obj_registro->{'launch_parameters'}];
     print 'OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK:';
-    echo $json_obj->{'data'}->{'0'}->{'launch_parameters'};
+    echo $json_obj->{'data'}->{'0'}->{'launch_parameters'}->{'0'};
 }
 fclose($stream);
 
