ca239d5141fd1aee495288f1bb8f017124a106a1
FJ <fsanchez@lsi.uned.es>
2020-12-06 18:30:35 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/db/lti_database.php
+++ src/db/lti_database.php
@@ -7,6 +7,57 @@ use \IMSGlobal\LTI;
 
 $_SESSION['iss'] = [];
 
+// TODO Conectar con servicio READ
+// Información servidor
+///////////////////////
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $url = "https://";
+else
+    $url = "http://";
+// Append the host(domain name, ip) to the URL.
+$url.= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$url.= $_SERVER['REQUEST_URI'];
+
+echo $_REQUEST['target_link_uri'];
+// Llamadas REST
+//  https://stackoverflow.com/questions/2445276/how-to-post-data-in-php-using-file-get-contents
+//  https://www.php.net/manual/en/context.http.php
+///////////////////
+$url = "http://10.201.54.31:49151/servicios/lti/lti13/read/5fc3860a81740b0ef098a965";
+
+$opts = array('http' =>
+    array(
+        'method' => 'GET',
+        'max_redirects' => '0',
+        'ignore_errors' => '1'
+    )
+);
+
+$context = stream_context_create($opts);
+$stream = fopen($url, 'r', false, $context);
+
+// header information as well as meta data
+// about the stream
+var_dump(stream_get_meta_data($stream));
+
+// actual data at $url
+//var_dump(stream_get_contents($stream));
+// Resultado
+$json_obj = json_decode(stream_get_contents($stream), true);
+var_dump($json_obj);
+echo $json_obj['result'];
+//echo $json_obj->{'data'}->{'usuario'}->{'email'};
+if($json_obj['result'] === "ok"){
+    // Registro
+    //echo "<p>" . 'SERVICIO GET:';
+    //print $json_obj['data']['launch_parameters']['iss'];
+    //print "<p>" . 'ARRAY ISS:';
+    $_SESSION['iss'] = [$json_obj['data']['launch_parameters']['iss'] => $json_obj['data']['credentials']];
+    //var_dump($_SESSION['iss']);
+}
+fclose($stream);
 
 // Obtiene la configuración de los sitios del directorio `/configs` y de fichero JSON
 $reg_configs = array_diff(scandir(__DIR__ . '/configs'), array('..', '.', '.DS_Store'));
