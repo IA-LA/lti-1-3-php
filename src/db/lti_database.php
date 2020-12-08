8062747e87f4fe1949bbb45eb8588c7b5236c3ed
FJ <fsanchez@lsi.uned.es>
2020-12-08 13:51:22 +0100

    LTI: conectar con servicio READ de la BBDD

--- src/db/lti_database.php
+++ src/db/lti_database.php
@@ -86,7 +86,7 @@ foreach ($reg_configs as $key => $reg_config) {
 class Lti_Database implements LTI\Database {
     public function find_registration_by_issuer($iss) {
         if (empty($_SESSION['iss']) || empty($_SESSION['iss'][$iss])) {
-            echo '<p>FRBI:' . $iss . '-' . $_SESSION['iss'][$iss];
+            echo '<p>FRBI:' . $iss . '-' . $_SESSION['iss']['5fc3860a81740b0ef098a965'];
             return false;
         }
         return LTI\LTI_Registration::new()
