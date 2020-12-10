ddcaeab3c0b6e3f02b6c988cfae68487ba7109f0
FJ <fsanchez@lsi.uned.es>
2020-12-10 15:45:00 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/web/login.php
+++ src/web/login.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../db/lti_database.php';
 
 //echo TOOL_HOST . "/launch.php" . "?target_link_uri=" . TOOL_REDIR;
-echo $_REQUEST['iss'], $_REQUEST['target_link_uri'];
+//echo $_REQUEST['iss'], $_REQUEST['target_link_uri'];
 
 use \IMSGlobal\LTI;
 
