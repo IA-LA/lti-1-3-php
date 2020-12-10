a6ba895d188e8189695eefefb62427ae2be8b9ab
FJ <fsanchez@lsi.uned.es>
2020-12-10 18:58:17 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/web/launch.php
+++ src/web/launch.php
@@ -9,19 +9,7 @@ $launch = LTI\LTI_Message_Launch::new(new Lti_Database())
 
 // IFRAME FULL PAGE cross-browser and fully responsive
 //  https://stackoverflow.com/questions/17710039/full-page-iframe
-echo '<iframe id="frame" src="' . $_REQUEST['target_link_uri'] . '"   style="
-    position: fixed;
-    top: 0px;
-    bottom: 0px;
-    right: 0px;
-    width: 100%;
-    border: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    z-index: 999999;
-    height: 100%;
-  "></iframe> <p>VARIABLES:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'];
+echo '<p>VARIABLES:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'];
 
 ?>
 
