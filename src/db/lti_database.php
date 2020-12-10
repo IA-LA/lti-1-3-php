0fcec27026c43555203cf0a552f0e55cd1a379df
FJ <fsanchez@lsi.uned.es>
2020-12-10 11:10:41 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/db/lti_database.php
+++ src/db/lti_database.php
@@ -6,7 +6,7 @@ use Firebase\JWT\JWT;
 
 define("TOOL_HOST", ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?: $_SERVER['REQUEST_SCHEME']) . '://' . $_SERVER['HTTP_HOST']);
 define("TOOL_REDIR", ($_REQUEST['target_link_uri'] ? $_REQUEST['target_link_uri'] : $_REQUEST['redirect_uri']) ); //explode('%26', explode('target_link_uri%3D', $_SERVER['REQUEST_URI'])[0]))[0]
-define("TOOL_ISS", ($_REQUEST['iss'] ? $_REQUEST['iss'] : $_REQUEST['id_token'] . explode('%26', explode('iss%3D', $_SERVER['REQUEST_URI'])[1])[0]) ); //$_POST['id_token'] $_REQUEST['state'] json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]))['aud'])
+define("TOOL_ISS", ($_REQUEST['iss'] ? $_REQUEST['iss'] : explode('.',$_REQUEST['id_token'])[1] . explode('%26', explode('iss%3D', $_SERVER['REQUEST_URI'])[1])[0]) ); //$_POST['id_token'] $_REQUEST['state'] json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]))['aud'])
 session_start();
 use \IMSGlobal\LTI;
 
