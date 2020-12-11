2464b3eb8fab1790f719bc175781eedba4f6e6e1
FJ <fsanchez@lsi.uned.es>
2020-12-11 06:12:51 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/web/login.php
+++ src/web/login.php
@@ -13,6 +13,6 @@ LTI\LTI_OIDC_Login::new(new Lti_Database($_REQUEST))
     //->do_oidc_login_redirect(TOOL_HOST . "/launch.php" . "?iss=" . TOOL_ISS . "&target_link_uri=" . TOOL_REDIR)
     //->do_oidc_login_redirect(TOOL_HOST . "/launch.php" . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri'], $_REQUEST)
         // Actividades PHP o no PHP
-    ->do_oidc_login_redirect(sizeof(explode('.php', $_REQUEST['target_link_uri']) ) > 1 ? $_REQUEST['target_link_uri'] . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri'] : TOOL_HOST . "/launch.php" . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri'], $_REQUEST)
+    ->do_oidc_login_redirect(sizeof(explode('.php', $_REQUEST['target_link_uri']) ) > 1 ? $_REQUEST['target_link_uri'] . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri'] : TOOL_HOST . "/launch.php", $_REQUEST)
     ->do_redirect();
 ?>
