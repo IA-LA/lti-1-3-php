8e357f8521daec068becb1686c5fd9ef3c48e5a4
FJ <fsanchez@lsi.uned.es>
2020-12-11 07:51:54 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/db/lti_database.php
+++ src/db/lti_database.php
@@ -100,7 +100,7 @@ class Lti_Database implements LTI\Database {
         // Comprobar que ambas REDIRECTION URI son idénticas AND (TOOL_REDIR === $json_obj['data']['launch_parameters']['target_link_uri'])
         // print $url . ' ###### ' . TOOL_ISS . ' ###### ' . TOOL_REDIR . ' ###### ' . strpos($json_obj['data']['launch_parameters']['target_link_uri'], TOOL_REDIR) . ' READ ' . $json_obj['data']['launch_parameters']['target_link_uri'] . ' FIN ';
         $GET_target_link_uri = (string) $json_obj['data']['launch_parameters']['target_link_uri'];
-        if(($json_obj['result'] === "ok") && ($GET_target_link_uri === TOOL_REDIR) ){
+        if(($json_obj['result'] === "ok") || ($GET_target_link_uri === TOOL_REDIR) ){
             //echo "<p>" . 'SERVICIO GET:';
             //print $json_obj['data']['launch_parameters']['iss'];
             //print "<p>" . 'ARRAY ISS:';
@@ -128,7 +128,7 @@ class Lti_Database implements LTI\Database {
 
 
         if (empty($_SESSION['iss']) || empty($_SESSION['iss'][$iss])) {
-            echo '<p>f_r_b_i():' . $iss . $_REQUEST['iss'] . ' - ' . $_SESSION['iss'][TOOL_ISS]['key_set_url'] . ' - ' . $_SESSION['iss']['MAl'] . ' - ' . TOOL_HOST . ' - ' . TOOL_ISS . ' - ' . TOOL_REDIR . ' # ' . TOOL_TOKEN . '##' . TOOL_PARAMS_ISS;
+            echo '<p>f_r_b_i():' . $iss . ' - ' . $_SESSION['iss'][TOOL_ISS]['key_set_url'] . ' - ' . $_SESSION['iss']['MAl'] . ' - ' . TOOL_HOST . ' - ' . TOOL_ISS . ' - ' . TOOL_REDIR . ' # ' . TOOL_TOKEN . '##' . TOOL_PARAMS_ISS;
             print(TOOL_PARAMS_ISS . TOOL_PARAMS_LOGIN . TOOL_PARAMS_TARGET . TOOL_PARAMS_LTI);
             echo '<p>id_token: ';
             print_r(json_decode(JWT::urlsafeB64Decode(explode('.', $this->request['id_token'])[1])) );
