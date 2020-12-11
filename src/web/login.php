67e1f7bf6f61f1dbb3f00b1d92bc1f5cc3e38034
FJ <fsanchez@lsi.uned.es>
2020-12-11 06:54:47 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/web/login.php
+++ src/web/login.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../db/lti_database.php';
 
 use \IMSGlobal\LTI;
 
-LTI\LTI_OIDC_Login::new(new Lti_Database($_REQUEST))
+LTI\LTI_OIDC_Login::new(new Lti_Database(["iss" => $_REQUEST['iss'], "login_hint" => $_REQUEST['login_hint'], "target_link_uri" => $_REQUEST['target_link_uri'], "lti_message_hint" => $_REQUEST['lti_message_hint']]))
     //->do_oidc_login_redirect(TOOL_HOST . "/game.php")
     //->do_oidc_login_redirect(TOOL_HOST . "https://ailanto-dev.intecca.uned.es/lti/launch.php" . "?target_link_uri=" . TOOL_REDIR)
     //->do_oidc_login_redirect(TOOL_HOST . "/launch.php" . "?iss=" . TOOL_ISS . "&target_link_uri=" . TOOL_REDIR)
