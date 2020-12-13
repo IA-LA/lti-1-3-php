9a4479ae74611066f47f9c8851ba395ea7e2503b
FJ <fsanchez@lsi.uned.es>
2020-12-13 21:31:57 +0100

    LTI: conectar con servicio READ de la BBDD. Actualización plataforma pruebas.

--- src/web/launch.php
+++ src/web/launch.php
@@ -14,7 +14,7 @@ use Firebase\JWT\JWT;
 $post_param = json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]), true);
 
 use \IMSGlobal\LTI;
-print_r($post_param);
+//print_r($post_param);
 $launch = LTI\LTI_Message_Launch::new(new Lti_Database(["iss" => TOOL_PARAMS_ISS, "login_hint" => $_REQUEST['login_hint'], "target_link_uri" => TOOL_PARAMS_TARGET, "lti_message_hint" => $_REQUEST['lti_message_hint']]))
     ->validate();
 
