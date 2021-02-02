6d26ea86040649c44284f5137b22855683bf4007
FJ <fsanchez@lsi.uned.es>
2021-02-02 13:11:29 +0100

    LTI: Ejemplo eContent directo sin BBDD.

--- src/web/platform/index.php
+++ src/web/platform/index.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../../db/lti_database.php';
     <!-- <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/login.php?iss=game&login_hint=12345&target_link_uri=<?php echo TOOL_HOST; ?>/game.php&lti_message_hint=12345'">Game</li> -->
     <!-- <li class="sub" onclick="document.getElementById('frame').src='http://10.201.54.31:9002/login_econtent.php?iss=http%3A%2F%2Flocalhost:9002&login_hint=123456&target_link_uri=http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43f/index.php&lti_message_hint=123456'">eContent</li> -->
     <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/game.php'">Game</li>
-    <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/Plantilla%20Azul_5e0df19c0c2e74489066b43f/index_default.html'">eContent</li>
+    <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/Plantilla%20Azul_5e0df19c0c2e74489066b43g/index.html'">eContent</li>
     <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/login.php?iss=5fc3860a81740b0ef098a968&login_hint=123456&target_link_uri=http://10.201.54.31:9002/game.php&lti_message_hint=123456'">Game BD</li>
     <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/login.php?iss=5fc3860a81740b0ef098a983&login_hint=123456&target_link_uri=http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43f/index_default.html&lti_message_hint=123456'">eContent BD</li>
     <li class="sub" onclick="document.getElementById('frame').src='<?php echo TOOL_HOST; ?>/login.php?iss=5fc3860a81740b0ef098a966&login_hint=123456&target_link_uri=https://www.uned.es&lti_message_hint=123456'">UNED BD</li>
