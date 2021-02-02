81737eb2aee6f31ca680feabb0e792c4d566d503
FJ <fsanchez@lsi.uned.es>
2021-02-02 09:58:06 +0100

    LTI: Ejemplo eContent directo sin BBDD.

--- src/web/login_econtent.php
+++ src/web/login_econtent.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../db/example_database.php';
 use \IMSGlobal\LTI;
 
 LTI\LTI_OIDC_Login::new(new Example_Database())
-    ->do_oidc_login_redirect(TOOL_HOST . "/Plantilla Azul_5e0df19c0c2e74489066b43f/index.php")
+    ->do_oidc_login_redirect(TOOL_HOST . "/Plantilla Azul_5e0df19c0c2e74489066b43f/index.php.default")
     ->do_redirect();
 ?>
