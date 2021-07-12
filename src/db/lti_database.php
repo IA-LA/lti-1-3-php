ed4ea5d39e334f90e642ecb98b00b6919c8802b6
FJ <fsanchez@lsi.uned.es>
2021-07-13 00:24:07 +0200

    LTI: Database. Servidor local y pre-producción.

--- src/db/lti_database.php
+++ src/db/lti_database.php
@@ -52,15 +52,15 @@ $opts = array('http' =>
     )
 );
 
-if(strpos(get_headers("http://10.201.54.31:49151/servicios/json/RUTAS.json", 0, $opts)[0], 'OK')){
+if(strpos(get_headers("http://10.201.54.31:49151/servicios/json/RUTAS.json", 0, stream_context_create($opts))[0], 'OK')){
     $url_get = "http://10.201.54.31:49151/servicios/lti/lti13/read/coleccion/Lti/id_actividad/" . TOOL_PARAMS_ISS;
     echo 'PARSE11: ' . parse_url("http://10.201.54.31:49151/servicios/json/RUTAS.json")['port'];
 }
-elseif (strpos(get_headers("http://192.168.0.31:49151/servicios/json/RUTAS.json", 0, $opts)[0], 'OK')) {
+elseif (strpos(get_headers("http://192.168.0.31:49151/servicios/json/RUTAS.json", 0, stream_context_create($opts))[0], 'OK')) {
     $url_get = "http://192.168.0.31:49151/servicios/lti/lti13/read/coleccion/Lti/id_actividad/" . TOOL_PARAMS_ISS;
     echo 'PARSE12: ' . parse_url('http://192.168.0.31:49151/servicios/lti/lti13/read/coleccion/Lti/id_actividad/');
 }
-elseif (strpos(get_headers("http://127.0.0.1:49151/servicios/json/RUTAS.json", 0, $opts)[0], 'OK')) {
+elseif (strpos(get_headers("http://127.0.0.1:49151/servicios/json/RUTAS.json", 0, stream_context_create($opts))[0], 'OK')) {
     $url_get = "http://127.0.0.1:49151/servicios/lti/lti13/read/coleccion/Lti/id_actividad/" . TOOL_PARAMS_ISS;
     echo 'PARSE13: ' . parse_url('http://127.0.0.1:49151/servicios/lti/lti13/read/coleccion/Lti/id_actividad/');
 }
