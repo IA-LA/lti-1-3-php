f3a3b0e877aec90e49e0615d2ac6f59e39d8c4cc
FJ <fsanchez@lsi.uned.es>
2021-09-08 19:14:48 +0200

    LTI: Token. Añade clase para salvar problemas con el fichero de token.

--- src/web/platform/token.php
+++ src/web/platform/token.php
@@ -3,8 +3,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../db/lti_database.php';
 
 use \IMSGlobal\LTI;
-use \Firebase\JWT\JWK;
-use \Firebase\JWT\JWT;
 
 function get_public_key($key_set_url) {
 
@@ -18,20 +16,20 @@ function get_public_key($key_set_url) {
 
     // Find key used to sign the JWT (matches the KID in the header)
     foreach ($public_key_set['keys'] as $key) {
-    //    if ($key['kid'] == $this->jwt['header']['kid'] && $key['alg'] == $this->jwt['header']['alg']) {
+        if ($key['kid'] == $this->jwt['header']['kid'] && $key['alg'] == $this->jwt['header']['alg']) {
             try {
                 return openssl_pkey_get_details(JWK::parseKey($key));
             } catch(\Exception $e) {
                 return false;
             }
-    //    }
+        }
     }
 
     // Could not find public key with a matching kid and alg.
     throw new LTI_Exception("Unable to find public key", 1);
 }
 
-$jwt = JWT::decode($_POST['jwt'], get_public_key(TOOL_HOST . "/jwks.php"), array('RS256'));
+//$jwt = JWT::decode($_POST['client_assertion'], get_public_key(TOOL_HOST . "/jwk.php"), array('RS256'));
 
 echo json_encode([
     'access_token' => '9a4b5056-cdce-4cdd-8981-053b610d0842'
