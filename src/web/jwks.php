f92cf11600c0a411ab6362c325e78410df43f11a
FJ <fsanchez@lsi.uned.es>
2021-10-20 17:29:43 +0200

    LTI: JWT. Añade JWT en formato JSON para salvar problemas con la autentificación con la Plataforma IMSGlobal.

--- src/web/jwks.php
+++ src/web/jwks.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../db/lti_database.php';
 use \IMSGlobal\LTI;
 
 LTI\JWKS_Endpoint::new([
-    'TRwtvqCcefOWuXU3-Dt4d26vCQExxh14vTO7_A375Pw' => file_get_contents(__DIR__ . '/../db/keys/platform/imsglobal_private.key')
+    'TRwtvqCcefOWuXU3-Dt4d26vCQExxh14vTO7_A375Pw' => file_get_contents(__DIR__ . '/../db/imsglobal_private.key')
 ])->output_jwks();
 
 ?>
