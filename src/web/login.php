d09c9130deaf69dccffc909ce551c9eeb51aee23
FJ <fsanchez@lsi.uned.es>
2022-03-23 20:40:43 +0100

    LTI: Login. Fija una dirección HTTPS.

--- src/web/login.php
+++ src/web/login.php
@@ -16,7 +16,7 @@ LTI\LTI_OIDC_Login::new(new Lti_Database(["iss" => $_REQUEST['iss'], "login_hint
         // eContent: utiliza un conteniedo .php y un iframe para presentarlo y manejar la llamda POST JWT LTI Claims (como la de platform/login.php)
         // Externas: se redireccionan directamente a su 'target_link_uri' de plataforma o actividad independiente.
     // Login en la Plataforma Consumidora de la Actividad LTI debidamente registrada 'target_link_uri'
-    ->do_oidc_login_redirect(sizeof(explode('econtent.php', $_REQUEST['target_link_uri']) ) > 1 ? ($_REQUEST['target_link_uri'] . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri']) : ("https://ailanto-dev.intecca.uned.es/lti13" . "/launch.php" . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri']), ["iss" => $_REQUEST['iss'], "login_hint" => $_REQUEST['login_hint'], "target_link_uri" => $_REQUEST['target_link_uri'], "lti_message_hint" => $_REQUEST['lti_message_hint']] )
+    ->do_oidc_login_redirect(sizeof(explode('econtent.php', $_REQUEST['target_link_uri']) ) > 1 ? ($_REQUEST['target_link_uri'] . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri']) : (TOOL_HOST . "/launch.php" . "?iss=" . $_REQUEST['iss'] . "&target_link_uri=" . $_REQUEST['target_link_uri']), ["iss" => $_REQUEST['iss'], "login_hint" => $_REQUEST['login_hint'], "target_link_uri" => $_REQUEST['target_link_uri'], "lti_message_hint" => $_REQUEST['lti_message_hint']] )
 
     // Redirección hacia 'target_link_uri'
         // https://www.w3docs.com/snippets/php/how-to-redirect-a-web-page-with-php.html
