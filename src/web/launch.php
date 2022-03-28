b5cd93b6f167e81c785225124f6805a5e321f1ca
FJ <fsanchez@lsi.uned.es>
2022-03-29 00:18:09 +0200

    LTI: Platform Login. Atributo ISS, REDIRECT_URI y LOGIN_HINT.      Database. Atributos HOST para la redirección HTTPS.      Launch. Atributos POST para la redirección HTTPS.      Login. Atributos POST/GET para la redirección HTTPS.

--- src/web/launch.php
+++ src/web/launch.php
@@ -8,15 +8,15 @@ use Firebase\JWT\JWT;
 use \IMSGlobal\LTI;
 try {
 
-    // Valida el Lanzamiento
-    // Lee los parámetros de la Redirección POST de la Plataforma
-    $launch = LTI\LTI_Message_Launch::new(new Lti_Database())
-        ->validate();
-
     // REDIRECCION POST
     // JWT Claims decode
     // https://auth0.com/blog/id-token-access-token-what-is-the-difference/
     $post_param = json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]), true);
+
+    // Valida el Lanzamiento
+    // Lee los parámetros de la Redirección POST de la Plataforma
+    $launch = LTI\LTI_Message_Launch::new(new Lti_Database())
+        ->validate();
 
     // GET
     //print_r($_REQUEST);
