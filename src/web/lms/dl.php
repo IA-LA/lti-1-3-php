7abb6af38e0581df2ab4fab5d26049a2f695dfca
FJ <fsanchez@lsi.uned.es>
2022-05-27 21:53:27 +0200

    LTI: Deep-Linking. Selector del tipo de contenido desde el LMS.

--- src/web/lms/dl.php
+++ src/web/lms/dl.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../db/iss_target_lti_database.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../db/iss_target_lti_database.php';
 
 use \IMSGlobal\LTI;
 try{
