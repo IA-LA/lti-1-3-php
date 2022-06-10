84820f9f405703c550d70d58c9fcb23c4532873e
FJ <fsanchez@lsi.uned.es>
2022-06-10 15:20:57 +0200

    LTI: Publish. SI Hace validación(). Message_Launch SI Pasa parámetros GET. Valida SI parámetros POST.      Launch. SI Hace validación().

--- src/web/lms/publish.php
+++ src/web/lms/publish.php
@@ -29,9 +29,10 @@ try {
     // Lee los parámetros de la Redirección POST de la Plataforma
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database(), null, $cookie);
     $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST))//;
+    //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($post_param))//;
         //->validate();
-        ->validate($_REQUEST);
-        //->validate($post_param);
+        //->validate($_REQUEST);
+        ->validate($post_param);
 
     // RELOCATION
     //header('X-Frame-Options: ' . 'SAMEORIGIN', true);
