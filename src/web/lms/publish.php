dff99e45e36a4ecb3a399162c60137542a83f796
FJ <fsanchez@lsi.uned.es>
2022-06-21 15:08:30 +0200

    LTI: Publish. SI Hace validación(). Message_Launch SI Pasa parámetros GET. Valida NO Pasa parámetros POST. Arregla Error 'Failed to fetch public key'. Oculta mensajes. Muestra modal.

--- src/web/lms/publish.php
+++ src/web/lms/publish.php
@@ -122,7 +122,22 @@ try {
 ?>
     <!-- Contenido -->
 
-    <p><br/></p>
+    <p>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    </p>
     <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenida al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ con perfil <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
 
 <?php
