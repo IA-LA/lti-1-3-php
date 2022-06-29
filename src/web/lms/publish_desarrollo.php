ad546d459daac32cb5b96be3e1e61440927d3ab1
FJ <fsanchez@lsi.uned.es>
2022-06-29 10:30:23 +0200

    LTI: Publish_desaroolo. Presenta primero NRPS y AGS.

--- src/web/lms/publish_desarrollo.php
+++ src/web/lms/publish_desarrollo.php
@@ -44,88 +44,8 @@ try {
         //->validate($_REQUEST);
         //->validate($post_param);
 
-    // RELOCATION
-    //header('X-Frame-Options: ' . 'SAMEORIGIN', true);
-    //header('Location: ' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"], true, 302);
-    //die;
-    
-    // GET
-    //print_r($_REQUEST);
-    //print('<p>' . $_REQUEST['iss']);
-    //print('<p>' . $_REQUEST['login_hint']);
-    //print('<p>' . $_REQUEST['target_link_uri']);
-    //print('<p>' . $_REQUEST['lti_message_hint']);
-    //print('<p>' . $_REQUEST['id_token']);
-    //print('<p>' . $_REQUEST['state']);
-    // POST
-    //print('<p>' . $post_param['iss'] . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"]);
-    //print_r($post_param);
-    //print('</p>');
-
-    // REDIRECTION HEADER
-    //header('Location: ' . TOOL_PARAMS_TARGET, true, 302);
-    //die;
-
-    // IFRAME FULL PAGE cross-browser and fully responsive
-    //  https://stackoverflow.com/questions/17710039/full-page-iframe
-    // ALTERNATIVES
-    //  https://www.geeksforgeeks.org/alternative-to-iframes-in-html5/
-    echo '
-        <!-- <embed id="frame1" src="https://ailanto-dev.intecca.uned.es/publicacion/' . $post_param['iss'] . '" -->
-        <embed id="embedPD" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]) . '"
-        style="
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        width: 100%;
-        border: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        z-index: 999999;
-        height: 100%;"/>
-        <!--
-        <iframe id="frame" src="' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . '"
-        allowfullscreen="true" allowpaymentrequest="true"
-        style="
-        position: fixed;
-        top: 0px;
-        bottom: 0px;
-        right: 0px;
-        width: 100%;
-        border: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        z-index: 999999;
-        height: 100%;"></iframe>
-        -->' .
-        '<!--',
-        '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
-        '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
-        '<hr/>',
-        '<br/><b>PLATFORM:</b> <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/tool_platform']['name'], '</a></b>',
-        '<hr/>',
-        '<br/><b>ISS: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', $post_param['iss'], '</a></b>',
-        '<br/><b>LOGIN_HINT: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', "no disponible", '</a></b>',
-        '<br/><b>TARGET_LINK_URI: <a href="http://Hecho.que.Lti_Database.tome.TARGET_LINK_URI.de.la.llamada.GET/POST">', $post_param['https://purl.imsglobal.org/spec/lti/claim/target_link_uri'], '</a></b>',
-        '<br/><b>LTI_MESSAGE_HINT: <a href="http://Hecho.que.Lti_Database.tome.LTI_MESSAGE_HINT.de.la.llamada.GET/POST">', $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"], '</a></b>',
-        '<br/><b>TYPE: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/message_type'], '</a></b>',
-        '<br/><b>VERSION: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/version'], '</a></b>',
-        '<br/><b>USER: <a href="http://">', $post_param['name'], '</a></b>',
-        '<br/><b>EMAIL: <a href="http://">', $post_param['email'], '</a></b>',
-        '<br/><b>ROL: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0], '</a></b>',
-        '-->'
-      ;
-
-?>
-    <!-- Contenido
-    <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenid@ al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ como <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
-    -->
-<?php
-
     //SERVICES
+    //////////
     // NPRS (Names and Role Provisioning Services)
     if (!$launch->has_nrps()) {
         throw new Exception("Don't have names and roles!");
@@ -244,6 +164,98 @@ try {
         echo json_encode($scoreboard);
     */
 
+?>
+    <!-- Contenido de JWT 1
+    <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenid@ al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ como <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
+    -->
+<?php
+
+    // REDIRECCTION
+    ///////////////
+    // RELOCATION
+    //header('X-Frame-Options: ' . 'SAMEORIGIN', true);
+    //header('Location: ' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"], true, 302);
+    //die;
+    
+    // GET
+    //print_r($_REQUEST);
+    //print('<p>' . $_REQUEST['iss']);
+    //print('<p>' . $_REQUEST['login_hint']);
+    //print('<p>' . $_REQUEST['target_link_uri']);
+    //print('<p>' . $_REQUEST['lti_message_hint']);
+    //print('<p>' . $_REQUEST['id_token']);
+    //print('<p>' . $_REQUEST['state']);
+    // POST
+    //print('<p>' . $post_param['iss'] . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"]);
+    //print_r($post_param);
+    //print('</p>');
+
+    // REDIRECTION HEADER
+    //header('Location: ' . TOOL_PARAMS_TARGET, true, 302);
+    //die;
+
+    // IFRAME FULL PAGE cross-browser and fully responsive
+    //  https://stackoverflow.com/questions/17710039/full-page-iframe
+    // ALTERNATIVES
+    //  https://www.geeksforgeeks.org/alternative-to-iframes-in-html5/
+    echo '
+        <!-- <embed id="frame1" src="https://ailanto-dev.intecca.uned.es/publicacion/' . $post_param['iss'] . '" -->
+        <embed id="embedPD" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]) . '"
+        style="
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        border: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        z-index: 999999;
+        height: 100%;"/>
+        <!--
+        <iframe id="frame" src="' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . '"
+        allowfullscreen="true" allowpaymentrequest="true"
+        style="
+        position: fixed;
+        top: 0px;
+        bottom: 0px;
+        right: 0px;
+        width: 100%;
+        border: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        z-index: 999999;
+        height: 100%;"></iframe>
+        -->' .
+        '<!--',
+        '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
+        '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
+        '<hr/>',
+        '<br/><b>PLATFORM:</b> <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/tool_platform']['name'], '</a></b>',
+        '<hr/>',
+        '<br/><b>ISS: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', $post_param['iss'], '</a></b>',
+        '<br/><b>LOGIN_HINT: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', "no disponible", '</a></b>',
+        '<br/><b>TARGET_LINK_URI: <a href="http://Hecho.que.Lti_Database.tome.TARGET_LINK_URI.de.la.llamada.GET/POST">', $post_param['https://purl.imsglobal.org/spec/lti/claim/target_link_uri'], '</a></b>',
+        '<br/><b>LTI_MESSAGE_HINT: <a href="http://Hecho.que.Lti_Database.tome.LTI_MESSAGE_HINT.de.la.llamada.GET/POST">', $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"], '</a></b>',
+        '<br/><b>TYPE: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/message_type'], '</a></b>',
+        '<br/><b>VERSION: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/version'], '</a></b>',
+        '<br/><b>USER: <a href="http://">', $post_param['name'], '</a></b>',
+        '<br/><b>EMAIL: <a href="http://">', $post_param['email'], '</a></b>',
+        '<br/><b>ROL: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0], '</a></b>',
+        '-->'
+      ;
+
+?>
+    <!-- Contenido de JWT 2
+    <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenid@ al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ como <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
+    -->
+<?php
+    //LAUNCH TYPE:
+    //  - LtiResourceLinkRequest
+    //  - LtiDeepLinkingRequest
+    //  - Otros tipos!!!
     if ($launch->is_resource_launch()) {
         // https://purl.imsglobal.org/spec/lti/claim/message_type ==== LtiResourceLinkRequest
         echo '<!-- <hr/><br/><b>Resource Link Request Launch!</b> -->';
