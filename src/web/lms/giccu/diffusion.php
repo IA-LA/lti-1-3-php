f47c828b8976b22e78f222f2d36af35035b0fc2e
FJ <fsanchez@lsi.uned.es>
2022-06-30 09:38:39 +0200

    LTI: Difusión. Presenta Wrapper php.ini, NRPS y AGS.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../db/iss_target_lti_database.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../db/iss_target_lti_database.php';
 
 use Firebase\JWT\JWK;
 use Firebase\JWT\JWT;
