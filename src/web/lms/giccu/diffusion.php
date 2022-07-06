210edd5f5d35a85520800b457a31b923c52b448b
FJ <fsanchez@lsi.uned.es>
2022-07-06 13:11:40 +0200

    LTI: Difusión. Distintas vistas Resource/Deep Link. NRPS y AGS.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -40,8 +40,8 @@ try {
     // Evita ERROR ´State not found´
     $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST))//;
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($post_param))//;
-        ->validate();
-        //->validate($_REQUEST);
+        //->validate();
+        ->validate($_REQUEST);
         //->validate($post_param);
 
 ?>
