85f4630d3b7293f3811e47dc6cb956fac16b8ac7
FJ <fsanchez@lsi.uned.es>
2022-07-07 12:06:07 +0200

    LTI: Difusión. Distintas vistas Resource/Deep Link. NRPS y AGS.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -81,7 +81,44 @@ try {
 
         $members = $launch->get_nrps()->get_members();
         echo '<br/><br/><b>MEMBERS1:</b>' . json_encode($members);
-        print_r($launch->get_nrps()->get_members());
+        print_r($members);
+
+        $score_lineitem = LTI\LTI_Lineitem::new()
+            ->set_tag('score')
+            ->set_score_maximum(100)
+            ->set_label('Score')
+            ->set_resource_id($launch->get_launch_data()['https://purl.imsglobal.org/spec/lti/claim/resource_link']['id']);
+
+        $scores = $grades->get_grades($score_lineitem);
+
+        $time_lineitem = LTI\LTI_Lineitem::new()
+            ->set_tag('time')
+            ->set_score_maximum(999)
+            ->set_label('Time Taken')
+            ->set_resource_id('time'.$launch->get_launch_data()['https://purl.imsglobal.org/spec/lti/claim/resource_link']['id']);
+        $times = $ags->get_grades($time_lineitem);
+
+        $members = $launch->get_nrps()->get_members();
+
+        $scoreboard = [];
+
+        foreach ($scores as $score) {
+            $result = ['score' => $score['resultScore']];
+            foreach ($times as $time) {
+                if ($time['userId'] === $score['userId']) {
+                    $result['time'] = $time['resultScore'];
+                    break;
+                }
+            }
+            foreach ($members as $member) {
+                if ($member['user_id'] === $score['userId']) {
+                    $result['name'] = $member['name'];
+                    break;
+                }
+            }
+            $scoreboard[] = $result;
+        }
+        echo json_encode($scoreboard);
 
         $grade = LTI\LTI_Grade::new()
             ->set_score_given(20)
