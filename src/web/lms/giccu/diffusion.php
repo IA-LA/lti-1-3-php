93fa7a29448cf58990fe8df36eba7836b1f1dcd4
FJ <fsanchez@lsi.uned.es>
2022-09-25 22:26:47 +0200

    LTI: Difusión. NRPS y AGS. Grade y lineitem.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -34,11 +34,11 @@ try {
 
     // Valida el Lanzamiento
     // Lee los parámetros de la Redirección POST de la Plataforma
-    //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database()) //;
+    $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database()) //;
     // Usa una Cookie pero falla al evitar el ERROR ´State not found´
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database(), null, $cookie);
     // Evita ERROR ´State not found´
-    $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST))//;
+    //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST))//;
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($post_param))//;
         ->validate();
         //->validate($_REQUEST);
