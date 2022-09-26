16ce951198c07145e5037d0a98d4a0200c1130d2
FJ <fsanchez@lsi.uned.es>
2022-09-26 09:37:26 +0200

    LTI: Deep Linking. Extraer JWT y launch_id.

--- src/web/lms/dl.php
+++ src/web/lms/dl.php
@@ -12,6 +12,7 @@ try{
     // https://auth0.com/blog/id-token-access-token-what-is-the-difference/
     $post_param = json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]), true);
     print('<p><b>JWT (state)</b>: ' . $_REQUEST['state'] . '</p>');
+    print('<p><b>JWT (state)</b>: ' . json_decode(JWT::urlsafeB64Decode(explode('.', $_REQUEST['id_token'])[1]['https://purl.imsglobal.org/spec/lti/claim/message_type']), true). '</p>');
     print('<p><b>JWT (id_token)</b>: ');
     print_r($post_param);
     print('</p>');
@@ -19,10 +20,10 @@ try{
 
 
     //CREA LAUNCH
-    $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database());//
+    $launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database())//;
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST));//
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($post_param));//
-    //->validate();
+    ->validate();//
 
     //LAUNCH ID
     ///////////
