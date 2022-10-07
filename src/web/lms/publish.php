fafa3e906188d8e3294e7f436771da12c70e4467
FJ <fsanchez@lsi.uned.es>
2022-10-07 11:47:26 +0200

    LTI: Publish. Publica el id_token como comentario al pie.

--- src/web/lms/publish.php
+++ src/web/lms/publish.php
@@ -125,7 +125,7 @@ try {
     <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenid@ al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ como <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
     -->
 <?php
-    echo('</--');
+    echo('<!--');
     echo('<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>');
     echo('ID_TOKEN:');
     print_r($post_param);
