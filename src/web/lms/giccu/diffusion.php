102125b649c111b4ccec8d62b8426ba094365328
FJ <fsanchez@lsi.uned.es>
2022-10-14 14:46:07 +0200

    LTI: Diffusion. Iframe cebado con ejemplo usando fetch().

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -61,7 +61,7 @@ try {
         //  https://www.geeksforgeeks.org/alternative-to-iframes-in-html5/
         echo '
         <!-- <embed id="frame1" src="https://ailanto-dev.intecca.uned.es/publicacion/' . $post_param['iss'] . '" -->
-        <embed id="embedLD" src=""
+        <embed id="embedLD" src="https://ailanto-dev.intecca.uned.es/publicacion/index.html"
         style="
         position: fixed;
         top: 0;
