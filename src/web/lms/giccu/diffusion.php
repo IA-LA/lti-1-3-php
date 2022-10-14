798c2e1c0d4a77968326d6e21c6599661cc40b7f
FJ <fsanchez@lsi.uned.es>
2022-10-14 16:47:12 +0200

    LTI: Diffusion. Iframe con cebado usando document().

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -98,7 +98,7 @@ try {
             document["getElementById"](idOne)["contentWindow"]["document"].write("<script type=\'text/javascript\'>location.href = \'' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . '\'\x3c/script>");
             
             async function getSrc() {
-              const res = await fetch("https://www.uned.es", {
+              const res = await fetch("https://ailanto-dev.intecca.uned.es", {
                 method: \'GET\',
                 headers: {
                   // Here you can set any headers you want
