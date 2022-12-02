3112c6a642482a5ebc075befc08b1aab82eca225
FJ <fsanchez@lsi.uned.es>
2022-12-02 14:22:05 +0100

    LTI: Publish. Quita redirección accesos a .PHP de inyección.      Embed. Gestiona redirección accesos a .PHP de inyección.

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -41,15 +41,15 @@ try {
     // Intenta evitar ERROR ´State not found´ en NAVEGACION PRIVADA
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($_REQUEST))//;
     //$launch = LTI\LTI_Message_Launch::new(new Iss_Target_Lti_Database($post_param))//;
-        ->validate();
-        //->validate($_REQUEST);
-        //->validate($post_param);
+    ->validate($_REQUEST);
+    //->validate($_REQUEST);
+    //->validate($post_param);
 
     // RELOCATION HEADER
     //header('X-Frame-Options: ' . 'SAMEORIGIN', true);
     //header('Location: ' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"], true, 302);
     //die;
-    
+
     // GET
     //print_r($_REQUEST);
     //print('<p>' . $_REQUEST['iss']);
@@ -71,9 +71,9 @@ try {
     //  https://stackoverflow.com/questions/17710039/full-page-iframe
     // ALTERNATIVES
     //  https://www.geeksforgeeks.org/alternative-to-iframes-in-html5/
-    echo '
-        <!-- <embed id="frame1" src="https://ailanto-dev.intecca.uned.es/publicacion/' . $post_param['iss'] . '" -->
-        <embed id="embedP" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]) . '"
+    // TODO+NE Incidencia `$_REQUEST is not defined`
+    // Creadas variables y parámetros para enviar al CLiente el JWT
+    echo '<embed id="embedE" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]). '/' . $_REQUEST['path'] . '?id_token='. $_REQUEST['id_token'] .'&state=' . $_REQUEST['state'] .'"
         style="
         position: fixed;
         top: 0;
@@ -103,28 +103,28 @@ try {
         height: 100%;"></iframe>
         -->' .
         '<!--',
-        '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
-        '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
-        '<hr/>',
-        '<br/><b>PLATFORM:</b> <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/tool_platform']['name'], '</a></b>',
-        '<hr/>',
-        '<br/><b>ISS: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', $post_param['iss'], '</a></b>',
-        '<br/><b>LOGIN_HINT: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', "no disponible", '</a></b>',
-        '<br/><b>TARGET_LINK_URI: <a href="http://Hecho.que.Lti_Database.tome.TARGET_LINK_URI.de.la.llamada.GET/POST">', $post_param['https://purl.imsglobal.org/spec/lti/claim/target_link_uri'], '</a></b>',
-        '<br/><b>LTI_MESSAGE_HINT: <a href="http://Hecho.que.Lti_Database.tome.LTI_MESSAGE_HINT.de.la.llamada.GET/POST">', $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"], '</a></b>',
-        '<br/><b>TYPE: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/message_type'], '</a></b>',
-        '<br/><b>VERSION: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/version'], '</a></b>',
-        '<br/><b>USER: <a href="http://">', $post_param['name'], '</a></b>',
-        '<br/><b>EMAIL: <a href="http://">', $post_param['email'], '</a></b>',
-        '<br/><b>ROL: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0], '</a></b>',
-        '-->'
-      ;
+    '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
+    '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
+    '<hr/>',
+    '<br/><b>PLATFORM:</b> <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/tool_platform']['name'], '</a></b>',
+    '<hr/>',
+    '<br/><b>ISS: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', $post_param['iss'], '</a></b>',
+    '<br/><b>LOGIN_HINT: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', "no disponible", '</a></b>',
+    '<br/><b>TARGET_LINK_URI: <a href="http://Hecho.que.Lti_Database.tome.TARGET_LINK_URI.de.la.llamada.GET/POST">', $post_param['https://purl.imsglobal.org/spec/lti/claim/target_link_uri'], '</a></b>',
+    '<br/><b>LTI_MESSAGE_HINT: <a href="http://Hecho.que.Lti_Database.tome.LTI_MESSAGE_HINT.de.la.llamada.GET/POST">', $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"], '</a></b>',
+    '<br/><b>TYPE: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/message_type'], '</a></b>',
+    '<br/><b>VERSION: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/version'], '</a></b>',
+    '<br/><b>USER: <a href="http://">', $post_param['name'], '</a></b>',
+    '<br/><b>EMAIL: <a href="http://">', $post_param['email'], '</a></b>',
+    '<br/><b>ROL: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0], '</a></b>',
+    '-->'
+    ;
 
-?>
+    ?>
     <!-- Contenido
     <p>Hola <?php echo $post_param["given_name"]; ?>, bienvenid@ al eContent ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["title"]; ?>´ del curso ´<?php echo $post_param["https://purl.imsglobal.org/spec/lti/claim/context"]["title"]; ?>´ como <?php echo explode('#', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0])[1]; ?> </p>
     -->
-<?php
+    <?php
     if ($launch->is_resource_launch()) {
         // https://purl.imsglobal.org/spec/lti/claim/message_type ==== LtiResourceLinkRequest
         echo '<!-- <hr/><br/><b>Resource Link Request Launch!</b> -->';
@@ -136,16 +136,16 @@ try {
     } else if ($launch->is_deep_link_launch()) {
         // https://purl.imsglobal.org/spec/lti/claim/message_type ==== LtiDeepLinkingRequest
         echo '<!-- <hr/><br/><b>Deep Linking Request Launch!</b> -->';
-?>
-    <!--
+        ?>
+        <!--
         <div id="config">
             <br/>LAUNCH DEEP LINK:
             <?php
-            echo $post_param['iss'], $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"];
-            ?>
+        echo $post_param['iss'], $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"];
+        ?>
         </div>
      -->
-<?php
+        <?php
         die;
     } else {
         // https://purl.imsglobal.org/spec/lti/claim/message_type ==== otros tipos
