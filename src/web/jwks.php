21cd3f1c6d74b83683b47cc3670681acd6ceeca3
FJ <fsanchez@lsi.uned.es>
2023-01-31 11:58:38 +0100

    LTI: JWKS. Nueva clave privada

--- src/web/jwks.php
+++ src/web/jwks.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use \IMSGlobal\LTI;
 
 LTI\JWKS_Endpoint::new([
-    'TRwtvqCcefOWuXU3-Dt4d26vCQExxh14vTO7_A375Pw' => file_get_contents(__DIR__ . '/../db/private.key')
+    'TRwtvqCcefOWuXU3-Dt4d26vCQExxh14vTO7_A375Pw' => file_get_contents(__DIR__ . '/../db/imsglobal_tool_private.key')
 ])->output_jwks();
 
 ?>
