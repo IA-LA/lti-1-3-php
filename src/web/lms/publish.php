0aeb40330af7bb56e8a2b74b8599d94bd9abb41d
FJ <fsanchez@lsi.uned.es>
2023-02-07 15:25:21 +0100

    LTI: Publish. Gestiona redirección iframe.

--- src/web/lms/publish.php
+++ src/web/lms/publish.php
@@ -82,7 +82,7 @@ try {
             });
         </script>
         <!-- <embed id="frame1" src="https://ailanto-dev.intecca.uned.es/publicacion/' . $post_param['iss'] . '" -->
-        <embed id="embedP" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]) . '"
+        <embed id="embedP" src="http://ailanto-dev.intecca.uned.es/publication?id=10220210903095251000000a&actividad=https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a"
         style="
         position: fixed;
         top: 0;
