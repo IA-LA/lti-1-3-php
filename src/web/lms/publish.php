aeb7a0a06c0aa0e7cc3b79f34a90dda8f8c85b27
FJ <fsanchez@lsi.uned.es>
2023-02-07 14:59:42 +0100

    LTI: Publish. Gestiona redirección iframe.

--- src/web/lms/publish.php
+++ src/web/lms/publish.php
@@ -91,7 +91,7 @@ try {
         overflow: hidden;
         z-index: 999999;
         height: 100%;"/>
-        <!--
+        
         <iframe id="frame" src="' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . '"
         allowfullscreen="true" allowpaymentrequest="true"
         style="
@@ -106,7 +106,7 @@ try {
         overflow: hidden;
         z-index: 999999;
         height: 100%;"></iframe>
-        -->' .
+        ' .
         '<!--',
         '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
         '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
