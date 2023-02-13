2e6c1d28eab805751ae5280aec4d3c726bb76b82
FJ <fsanchez@lsi.uned.es>
2023-02-13 16:22:48 +0100

    LTI: Diffusion. Llamada prueba Token y Bearer.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -291,7 +291,7 @@ try {
                     //'Authorization: Bearer ' . $this->get_access_token($scopes),
                     //'Authorization: Bearer ' . $access_tokens[$scope_key] = '383fbc2711788ea4cc3e8cd7b902c355', // Moodle Mobile Web Service
                     'Authorization: Bearer ' . '97c8ba884cb1886204b0346f4ac34367', // LTI Services
-                    'Accept:' . 'application/vnd.ims.lti-nrps.v2.membershipcontainer+json',
+                    'Accept:' . 'application/json',
                 ];
                 curl_setopt($ch, CURLOPT_URL, 'http://ailanto-dev.intecca.uned.es/mod/lti/services.php/CourseSection/2/bindings/3/memberships');
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
