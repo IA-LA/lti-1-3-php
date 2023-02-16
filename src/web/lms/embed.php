27e0b329a0772e97dede3811285c77b54b911058
FJ <fsanchez@lsi.uned.es>
2023-02-16 10:55:37 +0100

    LTI: Diffusion. Llamadas AGS GET Y POST.      Editor. Comenta versión operativa.      Error. NRPS.      Embed. Añade Json con 3 Token (JWT, AGS y NRPS)

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -223,39 +223,7 @@ try {
             z-index: 999999;
             height: 100%;"
             onload="' . $authTokenScript . '"/>
-            <!--
-            <iframe id="frame" src="' . $post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"] . '"
-            allowfullscreen="true" 
-            allowpaymentrequest="true" 
-            style="
-            position: fixed;
-            top: 0px;
-            bottom: 0px;
-            right: 0px;
-            width: 100%;
-            border: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            z-index: 999999;
-            height: 100%;"></iframe>
-            -->' .
-            '<!--',
-            '<p>VARIABLES GET:</p>', $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING'],
-            '<p>VARIABLES POST:</p>', $_POST['state'], $_POST['id_token'],
-            '<hr/>',
-            '<br/><b>PLATFORM:</b> <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/tool_platform']['name'], '</a></b>',
-            '<hr/>',
-            '<br/><b>ISS: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', $post_param['iss'], '</a></b>',
-            '<br/><b>LOGIN_HINT: <a href="http://Hecho.que.Lti_Database.tome.este.parámetro.ISS.de.la.llamada.GET/POST">', "no disponible", '</a></b>',
-            '<br/><b>TARGET_LINK_URI: <a href="http://Hecho.que.Lti_Database.tome.TARGET_LINK_URI.de.la.llamada.GET/POST">', $post_param['https://purl.imsglobal.org/spec/lti/claim/target_link_uri'], '</a></b>',
-            '<br/><b>LTI_MESSAGE_HINT: <a href="http://Hecho.que.Lti_Database.tome.LTI_MESSAGE_HINT.de.la.llamada.GET/POST">', $post_param["https://purl.imsglobal.org/spec/lti/claim/resource_link"]["id"], '</a></b>',
-            '<br/><b>TYPE: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/message_type'], '</a></b>',
-            '<br/><b>VERSION: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/version'], '</a></b>',
-            '<br/><b>USER: <a href="http://">', $post_param['name'], '</a></b>',
-            '<br/><b>EMAIL: <a href="http://">', $post_param['email'], '</a></b>',
-            '<br/><b>ROL: <a href="http://">', $post_param['https://purl.imsglobal.org/spec/lti/claim/roles'][0], '</a></b>',
-            '-->'
+ '
         ;
 
     ?>
