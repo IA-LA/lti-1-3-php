87a568c03d77b071f3b98c3878e749a3168f978b
FJ <fsanchez@lsi.uned.es>
2023-02-16 11:59:44 +0100

    LTI: Diffusion. Llamadas AGS GET Y POST.      Editor. Comenta versión operativa.      Error. NRPS.      Embed. Añade Json con 3 Token (JWT, AGS y NRPS)

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -192,11 +192,11 @@ try {
     //  https://www.geeksforgeeks.org/alternative-to-iframes-in-html5/
     // TODO+NE Incidencia `$_REQUEST is not defined`
     // Creadas variables y parámetros para enviar al CLiente el JWT
-    $authTokenData='{
+    $authTokenData='\'{
                         \'id_token\': \'' . $_REQUEST['id_token'] . '\',
-                        \'auth_token_nrps\': \'' . $resp . '\',
-                        \'auth_token_ags\': \'' . $resp_ags . '\'
-                      }';
+                        \'auth_token_nrps\': ' . $resp . ',
+                        \'auth_token_ags\': ' . $resp_ags . '
+                      }\'';
     $authTokenScript='function loadToken() {
                             var iframe = document.getElementById(\'embedE\');
                             var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
