0373063a570680e7ea2b208eb45623fc78b9888c
FJ <fsanchez@lsi.uned.es>
2023-02-17 09:47:12 +0100

    LTI: Diffusion. Llamadas AGS GET Y POST.      Editor. Comenta versión operativa.      Error. NRPS.      Embed. Añade Json con 3 Token (JWT, AGS y NRPS)

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -241,9 +241,9 @@ try {
                     iframe.appendChild(script);
                  }
                 //document.write(JSON.parse(document.getElementById("data").text)["id_token"]);
-                document.write("<body>: " + innerDoc);
-                document.write("<innerHTML>: " + iframeDocument);
-                document.write("<var $_REQUEST>: ");
+                //document.write("<body>: " + innerDoc);
+                //document.write("<innerHTML>: " + iframeDocument);
+                //document.write("<var $_REQUEST>: ");
             }
         </script>
         <embed id="embedE" src="' . ($post_param["https://purl.imsglobal.org/spec/lti/claim/target_link_uri"]) . '?id_token=' . $_REQUEST['id_token'] . '&state=' . $_REQUEST['state'] . '"
