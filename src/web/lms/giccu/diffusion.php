11e1b681deb8a9cdfd950e5507cd0eca17365be4
FJ <fsanchez@lsi.uned.es>
2023-02-20 21:25:37 +0100

    LTI: Diffusion. Llamadas AGS GET (resultcontainer),POST Y PUT (/scores).

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -402,7 +402,9 @@ try {
                 //'Accept:' . 'application/vnd.ims.lti-nrps.v2.membershipcontainer+json',
                 // AGS accept
                 //'Accept:' . 'application/vnd.ims.lis.v1.score+json', //POST
-                // TODO-NE https://www.imsglobal.org/sites/default/files/lti/ltiv2p1/model/mediatype/application/vnd/ims/lis/v1/scorecontainer+json/index.html#The_JSON-LD_Context
+                /**
+                 * https://www.imsglobal.org/sites/default/files/lti/ltiv2p1/model/mediatype/application/vnd/ims/lis/v1/scorecontainer+json/index.html#The_JSON-LD_Context
+                 */
                 //'Accept:' . 'application/vnd.ims.lis.v1.scorecontariner+json', //pruebas
                 //'Accept:' . 'application/vnd.ims.lis.v2.score+json', //pruebas
                 'Accept:' . 'application/vnd.ims.lis.v2.lineitem+json', //POST
@@ -429,10 +431,10 @@ try {
             if ($method === 'POST') {
                 curl_setopt($ch, CURLOPT_POST, 1);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, strval($body));
-                //$headers[] = 'Content-Type: ' . 'application/json';
-                //array_push($headers, 'Content-Type: ' . 'application/json');
                 // TODO Incidencia: HTTP/1.1 400 Incorrect score received
-                array_push($headers, 'Content-Type: application/vnd.ims.lis.v2.lineitem+json');
+                //$headers[] = 'Content-Type: ' . 'application/json';
+                array_push($headers, 'Content-Type: ' . 'application/json');
+                //array_push($headers, 'Content-Type: application/vnd.ims.lis.v2.lineitem+json');
             }
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             $response = curl_exec($ch);
