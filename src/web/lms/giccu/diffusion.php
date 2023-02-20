d115d9aa37668ef1563ae98d60419919d60857b4
FJ <fsanchez@lsi.uned.es>
2023-02-20 20:24:09 +0100

    LTI: Diffusion. Llamadas AGS GET (resultcontainer),POST Y PUT (/scores).                 Poner notas de prueba.

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -763,7 +763,7 @@ try {
                 $members = $launch->get_nrps()->get_members();
 
                 $scoreboard = [];
-                echo '<br/><br/><b>SCOREBOARDS(scores)</b>:' . json_encode($scores,$times,$members);
+                echo '<br/><br/><b>SCOREBOARDS(scores)</b>:' . json_encode($scores);
                 echo '<br/><br/><b>SCOREBOARDS(times)</b>:' . json_encode($times);
                 echo '<br/><br/><b>SCOREBOARDS(members)</b>:' . json_encode($members);
 
