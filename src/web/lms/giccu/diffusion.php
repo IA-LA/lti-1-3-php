4158a3e0029d6db03819c517a5513f4d80e0106a
FJ <fsanchez@lsi.uned.es>
2023-02-21 12:11:00 +0100

    LTI: Diffusion. Llamadas AGS GET(/results),POST(/lineitem y /scores) y PUT(/lineitem).

--- src/web/lms/giccu/diffusion.php
+++ src/web/lms/giccu/diffusion.php
@@ -725,8 +725,6 @@ try {
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HEADER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($body));
-            array_push($headers, 'Content-Type: application/json');
             if ($method === 'POST') {
                 curl_setopt($ch, CURLOPT_POST, 1);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, strval($body));
