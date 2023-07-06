c839f1da4602db64d6d53671d987e6fd5d2fb7a3
FJ <fsanchez@lsi.uned.es>
2023-07-06 10:59:03 +0200

    LTI: Embed. Refresh token AGS.

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -264,7 +264,7 @@ try {
 
             // Build auth token request headers
             $auth_request = [
-                'grant_type' => 'refresh_token',
+                'grant_type' => 'authorization_code',
                 'refresh_token' => $token_data_ags,
                 'client_assertion_type' => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
                 'client_assertion' => $jwt,
