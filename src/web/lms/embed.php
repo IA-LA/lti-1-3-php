d39a9cfe8b36f07fa7b8285ffe46110275c7afc5
FJ <fsanchez@lsi.uned.es>
2023-07-07 07:31:05 +0200

    LTI: Embed. Kid JSON.

--- src/web/lms/embed.php
+++ src/web/lms/embed.php
@@ -133,7 +133,7 @@ try {
             // $kid=[];
             // $kid[0]='ff25d970a021ff7cdad1';
             // Sign the JWT with our private key (given by the platform on registration) and the JWKS json kid
-            $jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/private.key'), 'RS256', 'TRwtvqCcefOWuXU3-Dt4d26vCQExxh14vTO7_A375Pw');
+            $jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/private.key'), 'RS256', json_decode(file_get_contents(__DIR__ . '/../jwks.json'), true)['keys'][0]['kid']);
             //$jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/tool.key'), 'RS256');
 
             // Build auth token request headers
@@ -198,7 +198,7 @@ try {
             // $kid=[];
             // $kid[0]='ff25d970a021ff7cdad1';
             // Sign the JWT with our private key (given by the platform on registration) and the JWKS json kid
-            $jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/private.key'), 'RS256', json_decode(file_get_contents(__DIR__ . '/../jwks.json'), true)['kid']);
+            $jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/private.key'), 'RS256', json_decode(file_get_contents(__DIR__ . '/../jwks.json'), true)['keys'][0]['kid']);
             //$jwt = JWT::encode($jwt_claim, file_get_contents(__DIR__ . '/../../db/tool.key'), 'RS256');
 
             // Build auth token request headers
